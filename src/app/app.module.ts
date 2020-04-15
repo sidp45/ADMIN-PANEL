a380cecbb301b3212d9d5940cff31e3b0b3de45c
siddharth singh <siddharth.singh@escalesolutions.com>
2020-04-16 00:24:22 +0530

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DefaultModule } from './layouts/default/default.module';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+   
+
+  
+   
+  
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DefaultModule
   ],
   providers: [],
   bootstrap: [AppComponent]
